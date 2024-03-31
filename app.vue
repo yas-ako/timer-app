05bda2f05678768ea4f3851292155d8d650799b7
yasako <105139975+yas-ako@users.noreply.github.com>
2024-04-01 00:31:08 +0900

    update :インストールするものはこれで最後 npm install -D prettier prettier-plugin-tailwindcss

--- app.vue
+++ app.vue
@@ -14,64 +14,70 @@
     <div>
       {{ new Date(elapsedTime).toISOString() }}
     </div>
-    <div v-for="data in formatMilliseconds(elapsedTime)" key="data">
-      <div>{{ data[0] }}</div>
-      <div>{{ data[1] }}</div>
+    <div class="flex flex-col">
+      <div
+        v-for="data in formatMilliseconds(elapsedTime)"
+        key="data"
+        class="w-1/4"
+      >
+        <div>{{ data[0] }}</div>
+        <div>{{ data[1] }}</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-let startTime = ref();
-let elapsedTime = ref(0);
-let timerInterval;
-const isActive = ref(false);
+  const startTime = ref();
+  const elapsedTime = ref(0);
+  let timerInterval;
+  const isActive = ref(false);
 
-function startTimer() {
-  startTime.value = Date.now() - elapsedTime.value;
-  isActive.value = true;
-  timerInterval = setInterval(updateTime, 100); // 1秒ごとに経過時間を更新
-}
-
-function pauseTimer() {
-  clearInterval(timerInterval);
-  isActive.value = false;
-}
-
-function stopTimer() {
-  clearInterval(timerInterval);
-  elapsedTime.value = 0;
-  startTime.value = null;
-}
-
-function updateTime() {
-  const now = Date.now();
-  elapsedTime.value = now - startTime.value;
-  // 経過時間を表示するなどの処理をここで行う
-}
-
-// ページが非アクティブになったときのイベントリスナー
-document.addEventListener("visibilitychange", function () {
-  if (document.visibilityState === "hidden") {
-    pauseTimer();
+  function startTimer() {
+    startTime.value = Date.now() - elapsedTime.value;
+    isActive.value = true;
+    timerInterval = setInterval(updateTime, 100); // 1秒ごとに経過時間を更新
   }
-});
 
-function formatMilliseconds(milliseconds) {
-  let seconds = Math.floor(milliseconds / 1000);
-  let hours = Math.floor(seconds / 3600);
-  seconds %= 3600;
-  let minutes = Math.floor(seconds / 60);
-  seconds %= 60;
+  function pauseTimer() {
+    clearInterval(timerInterval);
+    isActive.value = false;
+  }
 
-  return [
-    [padZero(hours), "h"],
-    [padZero(minutes), "m"],
-    [padZero(seconds), "s"],
-  ];
-}
+  function stopTimer() {
+    clearInterval(timerInterval);
+    elapsedTime.value = 0;
+    startTime.value = null;
+  }
 
-function padZero(num) {
-  return num.toString().padStart(2, "0");
-}
+  function updateTime() {
+    const now = Date.now();
+    elapsedTime.value = now - startTime.value;
+    // 経過時間を表示するなどの処理をここで行う
+  }
+
+  // ページが非アクティブになったときのイベントリスナー
+  document.addEventListener("visibilitychange", function () {
+    if (document.visibilityState === "hidden") {
+      pauseTimer();
+    }
+  });
+
+  function formatMilliseconds(milliseconds) {
+    let seconds = Math.floor(milliseconds / 1000);
+    const hours = Math.floor(seconds / 3600);
+    seconds %= 3600;
+    const minutes = Math.floor(seconds / 60);
+    seconds %= 60;
+
+    return [
+      [padZero(hours), "h"],
+      [padZero(minutes), "m"],
+      [padZero(seconds), "s"],
+    ];
+  }
+
+  function padZero(num) {
+    return num.toString().padStart(2, "0");
+  }
 </script>
